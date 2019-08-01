481eca741d6ce40e8337cfc93a2363897f7e2aa5
basanta79 <basanta79@gmail.com>
2019-08-01 09:49:42 +0200

    :sparkles: Task1 finished

--- Task1.py
+++ Task1.py
@@ -18,3 +18,32 @@ How many different telephone numbers are there in the records?
 Print a message:
 "There are <count> different telephone numbers in the records."
 """
+
+def task1(texts, calls):
+    telephone_number_list = {}
+
+    for record in texts:
+        if record[0] in telephone_number_list:
+            telephone_number_list[record[0]] = telephone_number_list.get(record[0]) + 1
+        else:
+            telephone_number_list[record[0]] = 1
+
+        if record[1] in telephone_number_list:
+            telephone_number_list[record[1]] = telephone_number_list.get(record[1]) + 1
+        else:
+            telephone_number_list[record[1]] = 1
+
+    for record in calls:
+        if record[0] in telephone_number_list:
+            telephone_number_list[record[0]] = telephone_number_list.get(record[0]) + 1
+        else:
+            telephone_number_list[record[0]] = 1
+
+        if record[1] in telephone_number_list:
+            telephone_number_list[record[1]] = telephone_number_list.get(record[1]) + 1
+        else:
+            telephone_number_list[record[1]] = 1
+
+    print 'There are {0} different telephone numbers in the records.'.format(len(telephone_number_list))
+
+task1(texts, calls)
