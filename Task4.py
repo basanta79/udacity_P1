aacb1aa09d9918b5232bf01a279c3b5140df5b82
basanta79 <basanta79@gmail.com>
2019-08-06 07:38:05 +0200

    :sparkles: task4.py finished

--- Task4.py
+++ Task4.py
@@ -25,3 +25,36 @@ Print a message:
 The list of numbers should be print out one per line in lexicographic order with no duplicates.
 """
 
+def task5(texts, calls):
+    telemarketers_list = {}
+    receiving_calls = {}
+    texting_list = {}
+    receiving_text = {}
+
+    for record in calls:
+        telemarketers_list[record[0]] = 1
+
+    for record in calls:
+        numbers_removed = 0
+        numbers_removed += telemarketers_list.pop(record[1], 0)
+
+    for record in texts:
+        numbers_removed = 0
+        numbers_removed += telemarketers_list.pop(record[0], 0)
+        numbers_removed += telemarketers_list.pop(record[1], 0)
+
+    telemarketers_list_tuple = tuple(telemarketers_list)
+
+    print('These numbers could be telemarketers: {0}'.format(printTuple(telemarketers_list_tuple, False)))
+
+def printTuple(tuple_to_print, with_brackets):
+    string_result = ''
+    for element in tuple_to_print:
+        if with_brackets:
+            string_result = string_result + '\n' + '(' + str(element) + ')'
+        else:
+            string_result = string_result + '\n' + str(element)
+    return string_result
+
+
+task5(texts, calls)
